36f73559952247109a8aa1abb40b8a08a6859e59
ARJITH.S.R <proarjith23@gmail.com>
2026-05-29 13:29:47 +0530

    Delete Variables.java

--- Variables.java
+++
@@ -1,8 +0,0 @@
-class Variables{
-    public static void main(String args[]){
-        int age = 12;
-        String Name = "THALA";
-        System.out.println("Name :" + Name);
-        System.out.println("Age :" + age);
-    }
-}
