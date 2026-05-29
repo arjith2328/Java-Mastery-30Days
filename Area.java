a8ff06872bcee5f7549baa880aed2a56f69e138f
ARJITH.S.R <proarjith23@gmail.com>
2026-05-29 13:28:58 +0530

    Delete Area.java

--- Area.java
+++
@@ -1,8 +0,0 @@
-class Area{
-    public static void main(String args[]){
-        int length = 20;
-        int width = 34;
-        int area = length + width;
-        System.out.println("Area = " + area);
-    }
-}
