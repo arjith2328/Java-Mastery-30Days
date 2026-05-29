38112f2a1ba3c3e18352e17e19a4ee2e6ba1dd35
ARJITH.S.R <proarjith23@gmail.com>
2026-05-29 16:24:24 +0530

    Day 2 completed - Variables and Data Types
    Learned Java variables and data types.
    Created programs for addition, area calculation, student details, and swapping numbers.
    Completed Day 2 of my 30 Days Java Challenge.

---
+++ Day02_Variables/StudentDetails.java
@@ -0,0 +1,10 @@
+class StudentDetails{
+    public static void main(String args[]){
+        int regno = 23458;
+        String name = "THALA";
+        double cgpa = 9.65;
+        System.out.println("Reg.NO =" + regno);
+        System.out.println("Name =" + name);
+        System.out.println("CGPA =" + cgpa);
+    }
+}
---
+++ Day02_Variables/Variables.java
@@ -0,0 +1,8 @@
+class Variables{
+    public static void main(String args[]){
+        int age = 12;
+        String Name = "THALA";
+        System.out.println("Name :" + Name);
+        System.out.println("Age :" + age);
+    }
+}
