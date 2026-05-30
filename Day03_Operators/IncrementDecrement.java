592f0ecdc7e39e8be21ebb63201562000013981c
ARJITH.S.R <proarjith23@gmail.com>
2026-05-30 19:34:38 +0530

    Day 3 completed - Java Operators
    Learned Arithmetic, Relational, Logical, Increment and Decrement Operators.
    Built a simple calculator and practiced comparison and logical operations.
    Completed Day 3 of my 30 Days Java Challenge.

---
+++ Day03_Operators/IncrementDecrement.java
@@ -0,0 +1,10 @@
+class IncrementDecrement{
+    public static void main(String args[]){
+        int a = 10;
+        a++;
+        System.out.println(a);
+        a--;
+        System.out.println(a);
+
+    }
+}
---
+++ Day03_Operators/RelationalOperators.java
@@ -0,0 +1,13 @@
+class RelationOperators{
+    public static void main(String args[]){
+        int a = 34;
+        int b = 31;
+
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a<b);
+        System.out.println(a>b);
+        System.out.println(a<=b);
+        System.out.println(a>=b);
+    }
+}
