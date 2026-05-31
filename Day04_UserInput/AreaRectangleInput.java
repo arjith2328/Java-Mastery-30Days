4f69b9d0fa529293e123eb9ea6a8f486fee11195
ARJITH.S.R <proarjith23@gmail.com>
2026-05-31 11:04:50 +0530

    Day 4 completed - User Input using Scanner 🚀
    Learned Scanner class and user input handling.
    Created programs for addition, student details, area calculation, and calculator using user input.
    Completed Day 4 of my 30 Days Java Challenge.

---
+++ Day04_UserInput/AreaRectangleInput.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class AreaRectangleInput{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Length: ");
+        int length = sc.nextInt();
+
+        System.out.print("Enter Width: ");
+        int width = sc.nextInt();
+
+        int area = length *width;
+        System.out.println("Area of Rectangle= " + area);     
+    }
+}
