f4c15b1ac8702eaa547d6d27660c7e39fbcf697e
ARJITH.S.R <proarjith23@gmail.com>
2026-06-06 20:44:09 +0530

    Day 10 completed - Methods (Functions) 🚀
    Learned Java Methods, Parameters, Return Values, and Reusable Code.
    Built programs using methods for greetings, addition, return values, and finding the largest number.
    Completed Day 10 of my 30 Days Java Challenge.

---
+++ Day10_Methods/AdditionMethod.java
@@ -0,0 +1,10 @@
+class AdditionMethod{
+    static void add(int a, int b){
+        int sum = a + b;
+        System.out.println("Sum = " + sum);
+    }
+    public static void main(String args[]){
+        add(2 , 4);
+        add(67 ,78);
+    }
+}
